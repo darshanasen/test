1082fdd971f1937557b348c374b885a99db9e00d
Darshana Sen <darsh.sen@gmail.com>
2022-03-22 10:11:11 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './App.css'
+
+import DisplayPhotos from "./DisplayPhotos";
+import PhotoForm from "./PhotoForm";
 
 function App() {
+  const [allPhotos, setAllPhotos] = useState([]);
+  const [photoOrientation, setPhotoOrientation] = useState(null);
+  
+  useEffect(() => {
+
+    if (photoOrientation !== null) {
+
+      axios({
+        url: "https://api.unsplash.com/search/photos",
+        method: "GET",
+        dataResponse: "json",
+        params: {
+          client_id: "SPWoYE3TRrK7rkvIR2uFqa0RuCJ-tfaeJvWqFzdyd-w",
+          query: "puppies",
+          per_page: 30,
+          orientation: photoOrientation
+        },
+      }).then((res) => {
+  
+        setAllPhotos(res.data.results);
+      });
+
+    }
+
+  }, [photoOrientation]);
+
+  const getPhotos = (e, orientation) => {
+    e.preventDefault();
+
+    setPhotoOrientation(orientation);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Show me puppies!</h1>
       </header>
+      <PhotoForm getPhotos={getPhotos} />
+      <DisplayPhotos photos={allPhotos} />
     </div>
   );
 }
